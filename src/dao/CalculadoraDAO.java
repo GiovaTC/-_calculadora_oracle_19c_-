66dbc261e5fed227a7117106a2db97c6d78566ca
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-19 14:29:16 -0500

    commit - CalculadoraDAO .

--- src/dao/CalculadoraDAO.java
+++ src/dao/CalculadoraDAO.java
@@ -11,7 +11,38 @@ public class CalculadoraDAO {
     public void guardarOperacion(Operacion op) {
 
         String sql = """
-                INSERT INTO OPERACIONES
+                INSERT INTO OPERACIONES_OP
+                (
+                    NUMERO1,
+                    NUMERO2,
+                    OPERACION,
+                    RESULTADO
+                )
+                VALUES
+                (
+                    ?,?,?,?
+                )
                 """;
+
+        try (
+                Connection cn =
+                        ConexionOracle.conectar();
+
+                PreparedStatement ps =
+                        cn.prepareStatement(sql)
+        ) {
+            ps.setDouble(1, op.getNumero1());
+            ps.setDouble(2, op.getNumero2());
+            ps.setString(3, op.getOperacion());
+            ps.setDouble(4, op.getResultado());
+
+            ps.executeUpdate();
+
+            System.out.println(
+                    "OPERACION almacenada correctamente ."
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+}   

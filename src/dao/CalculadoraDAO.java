dd9c5ceb2c2c94c3aa5a7ccde201d9ee627695e6
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-19 14:32:43 -0500

    commit - CalculadoraService / .

--- src/dao/CalculadoraDAO.java
+++ src/dao/CalculadoraDAO.java
@@ -45,4 +45,4 @@ public class CalculadoraDAO {
             e.printStackTrace();
         }
     }
-}   
+}
--- src/servicio/CalculadoraService.java
+++ src/servicio/CalculadoraService.java
@@ -1,4 +1,40 @@
 package servicio;
 
+import dao.CalculadoraDAO;
+import modelo.Operacion;
+
 public class CalculadoraService {
+
+    private final CalculadoraDAO dao =
+            new CalculadoraDAO();
+
+    public double sumar(double a, double b) {
+
+        double r = a + b;
+
+        dao.guardarOperacion(
+                new Operacion(
+                        a,
+                        b,
+                        "SUMA",
+                        r
+                )
+        );
+        return r;
+    }
+
+    public double restar(double a, double b) {
+        double r = a - b;
+
+        dao.guardarOperacion(
+                new Operacion(
+                        a,
+                        b,
+                        "RESTA",
+                        r
+                )
+        );
+
+        return r;
+    }   
 }

2d7528a69854b8ce030c82e450c5b760a913d7f2
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-19 14:24:48 -0500

    commit - script_OPERACIONES_OP .

--- src/dao/CalculadoraDAO.java
+++ src/dao/CalculadoraDAO.java
@@ -1,4 +1,17 @@
 package dao;
 
+import conexion.ConexionOracle;
+import modelo.Operacion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class CalculadoraDAO {
+
+    public void guardarOperacion(Operacion op) {
+
+        String sql = """
+                INSERT INTO OPERACIONES
+                """;
+    }
 }

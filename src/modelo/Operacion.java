a47231439a346dd034b1678f168d4c2bf31074b2
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-19 10:48:28 -0500

    commit - Operacion .

--- src/modelo/Operacion.java
+++ src/modelo/Operacion.java
@@ -1,4 +1,79 @@
 package modelo;
 
 public class Operacion {
+
+    private int id;
+    private double numero1;
+    private double numero2;
+    private String operacion;
+    private double resultado;
+
+    public Operacion() {
+
+    }
+
+    public Operacion(
+            double numero1,
+            double numero2,
+            String operacion,
+            double resultado) {
+
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.operacion = operacion;
+        this.resultado = resultado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    @Override
+    public String toString() {
+        return id +
+                " | " +
+                numero1 +
+                " | " +
+                numero2 +
+                " | " +
+                operacion +
+                " | " +
+                resultado;
+    }
 }
